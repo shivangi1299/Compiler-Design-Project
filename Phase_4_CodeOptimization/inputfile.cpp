900bef57136ac17f73f61ac496ac0844917d479e
Shivangi <shivangig078@gmail.com>
2020-04-25 16:25:11 +0530

    Code Optimization

---
+++ Phase_4_CodeOptimization/inputfile.cpp
@@ -0,0 +1,40 @@
+int main()
+{
+	float x,y;
+	x=5;
+	int z;
+	z=8;
+	int a,b,c;
+	a = 10;
+	b = 45-a/2;
+	c = b*(200/a+2);
+
+	/*if(x>=10)
+	{
+		y = a+b;
+		z = 10;
+	}
+	else
+	{
+		a=b;
+		cout<<"Successful";
+	}
+	for(y=10;y<=5;y++)
+	{
+		w=a*y;
+		--b;
+	}
+	int t;
+	while(x<10)
+	{ x++;
+		for(y=10; y>=10; y++)
+		{
+			x=x+y;}
+	}
+	--z;
+	cout<<x+y;
+	x=10;
+	int x;
+	x=2*3*6;*/
+	--a;
+}
